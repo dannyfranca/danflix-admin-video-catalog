51fdf7e48744d864ee0d0534dfbd5d709fb43068
Danny França <danny_vcf@hotmail.com>
2022-02-25 22:16:56 -0300

    feat: category entity creation

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,73 @@
+import {omit} from 'lodash'
+
 import { Category } from "./category"
 
 describe('Category Tests', () => {
   test('constructor of category', () => {
-    const category = new Category('Movie')
+    let category = new Category({ name: 'Movie' })
+    let created_at: Date
+
+    let props = omit(category.props, 'created_at')
+    expect(props).toStrictEqual({
+      name: 'Movie',
+      description: null,
+      is_active: true
+    })
+    expect(category.created_at).toBeInstanceOf(Date)
+
+    created_at = new Date()
+    category = new Category({
+      name: 'Movie',
+      description: 'Some description',
+      is_active: false,
+      created_at
+    })
+    expect(category.props).toStrictEqual({
+      name: 'Movie',
+      description: 'Some description',
+      is_active: false,
+      created_at
+    })
+
+    category = new Category({ name: 'Movie', description: 'Some description' })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      description: 'Some description'
+    })
+
+    category = new Category({ name: 'Movie', is_active: true })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      is_active: true
+    })
+
+    created_at = new Date()
+    category = new Category({ name: 'Another Movie', created_at })
+    expect(category.props).toMatchObject({
+      name: 'Another Movie',
+      created_at
+    })
+  })
+
+  test('getters of category', () => {
+    let category: Category
+    let created_at: Date
+
+    category = new Category({ name: 'Movie' })
     expect(category.name).toBe('Movie')
+    expect(category.description).toBe(null)
+    expect(category.is_active).toBe(true)
+    expect(category.created_at).toBeInstanceOf(Date)
+
+    category = new Category({ name: 'Movie', description: 'Some description' })
+    expect(category.description).toBe('Some description')
+
+    category = new Category({ name: 'Movie', is_active: false })
+    expect(category.description).toBe(null)
+    expect(category.is_active).toBe(false)
+
+    created_at = new Date()
+    category = new Category({ name: 'Movie', created_at })
+    expect(category.created_at).toBe(created_at)
   })
 })
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,49 @@
+import {SetOptional} from 'type-fest'
+
+export type CategoryProperties = {
+  name: string
+  description: string | null
+  is_active: boolean
+  created_at: Date
+}
+
+export type CategoryPropertiesInput = SetOptional<CategoryProperties, 'description' | 'is_active' | 'created_at'>
+
 export class Category {
-  constructor(public name: string) {
-    
+  private _name: string
+  private _description: string | null
+  private _is_active: boolean
+  private _created_at: Date
+
+  constructor(props: CategoryPropertiesInput) {
+    this._name = props.name
+    this._description = props.description ?? null
+    this._is_active = props.is_active ?? true
+    this._created_at = props.created_at ?? new Date()
+  }
+  
+  get props (): CategoryProperties {
+    return {
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at
+    }
+  }
+
+  get name () {
+    return this._name
+  }
+
+  get description () {
+    return this._description
+  }
+
+  get is_active () {
+    return this._is_active
+  }
+
+  get created_at () {
+    return this._created_at
   }
 }
