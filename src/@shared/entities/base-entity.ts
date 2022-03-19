81f5ba3c43a419981a51a9611c653ffc3376ce7c
Danny França <danny_vcf@hotmail.com>
2022-03-19 11:15:35 -0300

    feat: add shared base entity

---
+++ src/@shared/entities/base-entity.ts
@@ -0,0 +1,48 @@
+import { SetOptional } from 'type-fest';
+
+import UniqueEntityId from '../value-objects/unique-entity-id';
+
+export interface BaseEntityProperties {
+  id: UniqueEntityId;
+  created_at: Date;
+  deleted_at: Date | null;
+}
+
+export type BaseEntityPropertiesInput = SetOptional<
+  BaseEntityProperties,
+  'id' | 'created_at' | 'deleted_at'
+>;
+
+export interface PlainEntity {
+  id: string;
+  created_at: Date;
+  deleted_at: Date | null;
+}
+
+export class Entity {
+  public readonly id: UniqueEntityId;
+  protected _created_at: Date;
+  protected _deleted_at: Date | null;
+
+  constructor(props: BaseEntityPropertiesInput) {
+    this.id = props.id ?? new UniqueEntityId();
+    this._created_at = props.created_at ?? new Date();
+    this._deleted_at = props.deleted_at ?? null;
+  }
+
+  get plain(): PlainEntity {
+    return {
+      id: this.id.value,
+      created_at: this.created_at,
+      deleted_at: this.deleted_at,
+    };
+  }
+
+  get created_at() {
+    return this._created_at;
+  }
+
+  get deleted_at() {
+    return this._deleted_at;
+  }
+}
