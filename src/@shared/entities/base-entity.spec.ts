7b1bb37701bc3a71651ef7087a26e432ccaefbb7
Danny França <danny_vcf@hotmail.com>
2022-03-19 11:44:31 -0300

    tes: improve base entity and unique id tests

--- src/@shared/entities/base-entity.spec.ts
+++ src/@shared/entities/base-entity.spec.ts
@@ -1,32 +1,33 @@
 import UniqueEntityId from "../value-objects/unique-entity-id";
 import { BaseEntity, PlainBaseEntity } from "./base-entity";
 
-const strName = "Entity";
-
 describe("Entity Tests", () => {
-  test("entity constructor", () => {
+  test("entity plain object", () => {
     let entity = new BaseEntity({});
+    let id: UniqueEntityId;
     let created_at: Date;
-    const deleted_at = new Date();
-
-    expect(entity.created_at).toBeInstanceOf(Date);
-    expect(entity.deleted_at).toBeNull();
+    let deleted_at: Date;
 
     created_at = new Date();
     entity = new BaseEntity({
       created_at,
     });
-    expect(entity.plain).toMatchObject({
+    expect(entity.plain).toStrictEqual({
+      id: entity.id.value,
       created_at,
       deleted_at: null,
     } as PlainBaseEntity);
 
+    id = new UniqueEntityId();
     created_at = new Date();
+    deleted_at = new Date();
     entity = new BaseEntity({
+      id,
       created_at,
       deleted_at,
     });
-    expect(entity.plain).toMatchObject({
+    expect(entity.plain).toStrictEqual({
+      id: id.value,
       created_at,
       deleted_at,
     });
@@ -38,24 +39,12 @@ describe("Entity Tests", () => {
     const deleted_at = new Date();
 
     entity = new BaseEntity({ deleted_at });
+    expect(entity.id).toBeInstanceOf(UniqueEntityId);
     expect(entity.created_at).toBeInstanceOf(Date);
     expect(entity.deleted_at).toBeInstanceOf(Date);
 
     entity = new BaseEntity({ created_at });
     expect(entity.created_at).toBe(created_at);
     expect(entity.deleted_at).toBeNull();
-  });
-
-  test("id field", () => {
-    let entity: BaseEntity;
-    const uniqueId = new UniqueEntityId();
-
-    entity = new BaseEntity({});
-    expect(entity.id).toBeInstanceOf(UniqueEntityId);
-
-    entity = new BaseEntity({ id: uniqueId });
-    expect(entity.id).toBeInstanceOf(UniqueEntityId);
-    expect(entity.id).toBe(uniqueId);
-    expect(entity.id.value).toBe(uniqueId.value);
   });
 });
--- src/@shared/value-objects/unique-entity-id.spec.ts
+++ src/@shared/value-objects/unique-entity-id.spec.ts
@@ -1,12 +1,12 @@
-import InvalidUuidError from '../errors/invalid-uuid.error';
-import UniqueEntityId from './unique-entity-id';
+import InvalidUuidError from "../errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id";
 
-describe('Unique Id Unit Tests', () => {
-  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+describe("Unique Id Unit Tests", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
 
   beforeEach(() => validateSpy.mockClear());
 
-  it('should accept nothing passed in constructor', () => {
+  it("should accept nothing passed in constructor", () => {
     let uniqueId: UniqueEntityId | undefined;
 
     expect(() => (uniqueId = new UniqueEntityId())).not.toThrow();
@@ -14,16 +14,16 @@ describe('Unique Id Unit Tests', () => {
     expect(validateSpy).toHaveBeenCalled();
   });
 
-  it('should accept a valid uuid passed in constructor', () => {
-    const validUUID = '766a36f1-077f-4e18-ab6f-66aa2bad98a5';
+  it("should accept a valid uuid passed in constructor", () => {
+    const validUUID = "766a36f1-077f-4e18-ab6f-66aa2bad98a5";
     const uniqueId = new UniqueEntityId(validUUID);
 
     expect(uniqueId.value).toBe(validUUID);
     expect(validateSpy).toHaveBeenCalled();
   });
 
-  it('should throw error when id is invalid', () => {
-    const noUUID = 'abc';
+  it("should throw error when id is invalid", () => {
+    const noUUID = "abc";
 
     expect(() => new UniqueEntityId(noUUID)).toThrow(InvalidUuidError);
     expect(validateSpy).toHaveBeenCalled();
