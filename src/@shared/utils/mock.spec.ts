239b37479026c02b89571f70e786e873b286e44a
Danny França <danny_vcf@hotmail.com>
2022-03-19 12:46:22 -0300

    feat: add mock utils tests

---
+++ src/@shared/utils/mock.spec.ts
@@ -0,0 +1,29 @@
+import {
+  randomDate,
+  randomDescription,
+  randomName,
+  randomSentence,
+} from "./mock";
+
+describe("Mock utils", () => {
+  it("shoud generate a 3 words sentence", () => {
+    const sentence = randomSentence(3);
+    expect(sentence.split(" ").length).toBe(3);
+  });
+
+  it("shoud generate a 2 words sentence", () => {
+    const sentence = randomName();
+    expect(sentence.split(" ").length).toBe(2);
+  });
+
+  it("shoud generate a 10 words sentence", () => {
+    const sentence = randomDescription();
+    expect(sentence.split(" ").length).toBe(10);
+  });
+
+  it("shoud generate a random Date between 1970 and 2050", () => {
+    const date = randomDate();
+    expect(date.getFullYear()).toBeGreaterThanOrEqual(1969);
+    expect(date.getFullYear()).toBeLessThanOrEqual(2051);
+  });
+});
--- src/@shared/utils/mock.ts
+++ src/@shared/utils/mock.ts
@@ -2,11 +2,12 @@ import { Chance } from "chance";
 
 const chance = new Chance();
 
-export const randomName = () => chance.sentence({ words: 2 });
-export const randomDescription = (words = 10) => chance.sentence({ words });
+export const randomSentence = (words: number) => chance.sentence({ words });
+export const randomName = () => randomSentence(2);
+export const randomDescription = () => randomSentence(10);
 export const randomDate = () =>
   chance.date({
-    min: new Date(1960),
-    max: new Date(2018),
+    min: new Date(1970),
+    max: new Date(2050),
     string: false,
   }) as Date;
