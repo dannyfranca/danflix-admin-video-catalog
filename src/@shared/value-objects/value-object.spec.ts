f10201a81fb42b557364ef2a75c8751f21fa491f
Danny França <danny_vcf@hotmail.com>
2022-03-19 15:25:23 -0300

    feat: add toString to value object

--- src/@shared/value-objects/value-object.spec.ts
+++ src/@shared/value-objects/value-object.spec.ts
@@ -12,4 +12,30 @@ describe("Value object unit tests", () => {
     vo = new StubValueObject({ prop: "val" });
     expect(vo.value).toStrictEqual({ prop: "val" });
   });
+
+  it("should convert to string", () => {
+    let vo: StubValueObject;
+    const date = new Date();
+    const arrange = [
+      { received: "", expected: "" },
+      { received: "val", expected: "val" },
+      { received: undefined, expected: "undefined" },
+      { received: null, expected: "null" },
+      { received: true, expected: "true" },
+      { received: false, expected: "false" },
+      { received: date, expected: date.toString() },
+      { received: { prop: 1 }, expected: JSON.stringify({ prop: 1 }) },
+    ];
+
+    for (const { received, expected } of arrange) {
+      vo = new StubValueObject(received);
+      expect(vo + "").toBe(expected);
+    }
+  });
+
+  it("should be immutable", () => {
+    const vo = new StubValueObject({ prop: 1, deep: { prop: "2" } });
+    expect(() => (vo.value.prop = 2)).toThrowError();
+    expect(() => (vo.value.deep.prop = 1)).toThrowError();
+  });
 });
--- src/@shared/value-objects/value-object.ts
+++ src/@shared/value-objects/value-object.ts
@@ -1,11 +1,28 @@
+import { deepFreeze } from "../utils/objects";
+
 export default abstract class ValueObject<T = any> {
-  protected _value: T;
+  protected readonly _value: T;
 
   constructor(value: T) {
-    this._value = value;
+    this._value = deepFreeze(value);
   }
 
   get value(): T {
     return this._value;
   }
+
+  toString = () => {
+    if (typeof this.value !== "object" || this.value === null) {
+      try {
+        (this.value as any).toString();
+      } catch {
+        return this.value + "";
+      }
+    }
+
+    const strValue = (this.value as any).toString();
+    return strValue === "[object Object]"
+      ? JSON.stringify(this.value)
+      : strValue;
+  };
 }
