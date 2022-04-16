65e8a9a3929ca463b39e1305d63a4237c9dc6c68
Danny França <danny_vcf@hotmail.com>
2022-04-16 09:00:26 -0300

    feat: add validation error to serve as base error for validation rules

--- src/@shared/errors/invalid-uuid.error.ts
+++ src/@shared/errors/invalid-uuid.error.ts
@@ -1,4 +1,6 @@
-export default class InvalidUuidError extends Error {
+import ValidationError from './validation.error';
+
+export default class InvalidUuidError extends ValidationError {
   constructor(message?: string) {
     super(message ?? 'ID must be a valid UUID');
     this.name = 'InvalidUuidError';
